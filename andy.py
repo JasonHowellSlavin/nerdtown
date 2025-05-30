7ce63a71e4daa9fe29d7a3885f0295424fc96180
Jason Slavin <slavin.jhs@gmail.com>
2025-05-29 19:55:19 -0700

    Refactoring

--- andy.py
+++ andy.py
@@ -1,7 +1,6 @@
 
 import random
 
-
 #Lists
 adjective = ["small", "big", "cute", "sneaky", "unusual", "helpful", "mean", "red", "blue", "green"]   
 animals = ["pig", "cow", "chicken", "zebra", "crayfish", "jellyfish", "worm", "boar", "dragon", "bull"]
@@ -13,70 +12,63 @@ e_name_prefix = ["thoron", "kano", "bale", "kemen", "varda", "mereth", "rhos", "
 e_name_suffix = ["on", "ath", "rim", "or", "dil", "dur", "nil", "nur", "iel", "lin", "aelin"]
 place = ["in the back corner", "at a center table", "at the bar", "near the front door", "next to the hearth", "near the back exit", "in a booth"]
 
+# Initialization number: this number is what sets up the program
+num_patrons = random.randint(0, 60)
+
 #Code
 def randomWordFromList(list):
     # get the max length
     max = len(list) - 1
     # return a random number from 0 to list length, and capitalize the result
-    return list[random.randint(0, max)].capitalize()
-
-def randomWordFromList_u(list):
-    max = len(list) - 1
     return list[random.randint(0, max)]
-    
-def label_price(price):
+
+def removeWordFromList(list):
+    # get the max length
+    max = len(list) - 1
+    index = random.randint(0, max)
+
+    return list.pop(index)
+
+def label_price():
+    price = random.randint(1, 12)
+
     if price > 8:
-        return "expensive"
+        return {"price": price, "quality": "Expensive" }
     elif price >= 5:
-        return "fair"
+        return {"price": price, "quality": "Fair" }
     else:
-        return "cheap"
-    
-price = random.randint(1, 12)
-label = label_price(price).capitalize()
+        return {"price": price, "quality": "Cheap" }
 
-def crowd_size(guys):
-    if guys > 39:
-        return "packed"
-    elif guys >= 20:
-        return "fairly busy"
-    elif guys >= 1:
-        return "sparse"
+def elfish_bar_description():
+    location_1 = removeWordFromList(place)
+    location_2 = removeWordFromList(place)
+    name_1 = removeWordFromList(e_name_prefix) + randomWordFromList(e_name_suffix)
+    name_2 = removeWordFromList(e_name_prefix) + randomWordFromList(e_name_suffix)
+
+    return f"Patrons: {name_1}{location_1}, {name_2}{location_2}"
+
+def crowd_size(crowd):
+    elfishness = random.randint(0, 2)
+    packed_with_elves = elfish_bar_description()
+    many_patrons = "some dudes"
+    is_packed_with_elves = packed_with_elves if elfishness > 1 else many_patrons
+
+    if crowd > 39:
+        return {"popularity": "packed", "description": is_packed_with_elves}
+    elif crowd >= 20:
+        return {"popularity": "fairly busy", "description": "some dudes"}
+    elif crowd >= 1:
+        return {"popularity": "sparse", "description": "a few dudes"}
     else:
-        return "empty"
+        return {"popularity": "empty", "description":  "empty of dudes"}
 
-guys = random.randint(40, 50)
-random_index = random.randint(0, len(place) - 1)
-random_index2 = random.randint(0, len(place) - 2)
-random_index3 = random.randint(0, len(e_name_prefix) - 1)
-elfishness = random.randint(0, 2)
-size = crowd_size(guys).capitalize()
-bartender = randomWordFromList(name_prefix) + randomWordFromList_u(name_suffix)
-elf_name = e_name_prefix.pop(random_index3) + randomWordFromList_u(e_name_suffix)
-elf_name2 = randomWordFromList(e_name_suffix) + randomWordFromList_u(e_name_suffix)
-bartender_name = bartender
-random_word_1 = place.pop(random_index)
-random_word_2 = place.pop(random_index2)
-num_patrons = random.randint(0, 100)
-bartender_elf_name = elf_name
+bar_population = crowd_size(num_patrons)
+label = label_price()
+bartender_name = randomWordFromList(name_prefix).capitalize() + randomWordFromList(name_suffix)
   
-
-
-def patrons(guys):
-    if guys > 39 and elfishness > 1:
-        return "Patrons:" + " " + elf_name + random_word_2 + elf_name2 + random_word_1
-    elif guys >= 20:
-        return "some dudes"
-    elif guys >= 1:
-        return "a few dudes"
-    else:
-        return "empty of dudes"
-
 # Testing
-print("Welccome To: The", randomWordFromList(adjective) + " " + randomWordFromList(animals))
-print("Quality:", randomWordFromList(quality))
-print("Available Beer:", randomWordFromList(beers))
-print("Price:", price, "Copper", "-", label)
-print("Popularity:", guys, size)
-print(elfishness)
-print(patrons(guys))
+print("Welccome To: The", randomWordFromList(adjective).capitalize() + " " + randomWordFromList(animals).capitalize())
+print("Quality:", randomWordFromList(quality).capitalize())
+print("Available Beer:", randomWordFromList(beers).capitalize())
+print("Price:", label["price"], "Copper", "-", label["quality"])
+print(f"Popularity: {num_patrons} {bar_population['popularity']}, {bar_population['description']}")
